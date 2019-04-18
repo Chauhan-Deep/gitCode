05d628c05b4e3433cee095fdb3332fc07239942a
Yogesh Gulati <ygulati@quark.com>
2019-04-18 21:02:22 +0530

    Updated shop URL

--- TrialScreen/src/app/trial-screen/trial-screen.component.ts
+++ TrialScreen/src/app/trial-screen/trial-screen.component.ts
@@ -15,9 +15,12 @@ export class TrialScreenComponent implements OnInit {
 
   buyNow() {
     if ((<any>window).app) {
-      (<any>window).app.launchApp('https://shop.quark.com/default.aspx');
+      (<any>window).app.launchApp(
+        'https://shop.quark.com/default.aspx?utm_source=QXP_App&utm_medium=PopUp&utm_campaign=Trial_Conversion&utm_content=Countdown.v1');
     } else {
-      window.open('https://shop.quark.com/default.aspx', '_blank');
+      window.open(
+        'https://shop.quark.com/default.aspx?utm_source=QXP_App&utm_medium=PopUp&utm_campaign=Trial_Conversion&utm_content=Countdown.v1',
+        '_blank');
     }
   }
 
