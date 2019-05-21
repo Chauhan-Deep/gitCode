c8bdf86cf196127009906ab5e4d259b1db05a4d2
Yogesh Gulati <ygulati@quark.com>
2019-05-21 17:29:15 +0530

    Updated string.

--- customer-engagements/Feedback/src/app/translate/i18n/en-US.ts
+++ customer-engagements/Feedback/src/app/translate/i18n/en-US.ts
@@ -5,7 +5,7 @@ export const LANG_EN_US_TRANS = {
     'headerMessage': 'We are always looking for ways to improve QuarkXPress.<br />Please take a moment to tell us about your experience.',
     'mainHeader': 'How likely are you to recommend<br />QuarkXPress to a friend?',
     'placeholderText': 'What can we do to improve your experience? Your feedback will be passed directly to Product Management & Technical Support teams to help improve the product for everyone.',
-    'contactText': 'Contact me via email for more support or feedback',
+    'contactText': 'For additional details regarding my feedback, Quark can contact me via email',
     'emailPlaceholder': 'Your email address',
     'sendText': 'Send Feedback',
     'disclaimer': 'By giving us feedback, you agree that Quark can use your feedback to improve our products and services. <a (click)=\'openPrivacyPolicy()\' href=\'\'>Privacy Statement.</a>',
