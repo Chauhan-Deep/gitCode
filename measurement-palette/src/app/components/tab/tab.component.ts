659877d0589a295b3b15becffc7014888fcf1315
Rajat Sadana <rsadana@quark.com>
2019-07-26 19:32:29 +0530

    Vertical measurement properties block mock example
    Related Work Items: #89518

--- measurement-palette/src/app/components/tab/tab.component.ts
+++ measurement-palette/src/app/components/tab/tab.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { BlockSeparatorType } from '@quark/xpressng';
+import { Component, OnInit, ChangeDetectorRef} from '@angular/core';
+import { BlockSeparatorType, measurementUnitTypes } from '@quark/xpressng';
 
 @Component({
     selector: 'qrk-tab',
@@ -8,12 +8,54 @@ import { BlockSeparatorType } from '@quark/xpressng';
 })
 export class TabComponent implements OnInit {
     activeTabIndex = 1;
+    xpressEnv: boolean;
     BLOCK_SEPARATOR_TYPE = BlockSeparatorType.BELOW;
+    unit = measurementUnitTypes;
+    minValueAngle = -360 << 16;
+    maxValueAngle = 360 << 16;
+    minValueSkew = -75 << 16;
+    maxValueSkew = 75 << 16;
+
+    minValueLength = this.xpressEnv ? 0 << 16 : 0;
+    maxValueLength = this.xpressEnv ? 10000 << 16 : 240;
+
+    currentValueAngle = '30°';
+    currentValueSkew = '30°';
+    currentValueOriginX;
+    currentValueOriginY;
+    currentValueWidth = '2in';
+    currentValueHeight = '3in';
+
+    constructor(private changeDetectorRef: ChangeDetectorRef) { }
 
     ngOnInit() {
+        this.xpressEnv = (<any> window).app ? true : false;
+
+        const callbackListener = (response) => {
+             response = JSON.parse(response);
+
+             this.currentValueOriginX = response.positionX ? response.positionX : this.currentValueOriginX;
+             this.currentValueOriginY = response.positionY ? response.positionY : this.currentValueOriginY;
+
+             this.currentValueHeight = response.height ? response.height : this.currentValueHeight;
+             this.currentValueWidth = response.width ? response.width : this.currentValueWidth;
+
+             this.currentValueAngle = response.angle ? response.angle : this.currentValueAngle;
+             this.currentValueSkew = response.skew ? response.skew : this.currentValueSkew;
+
+             this.changeDetectorRef.detectChanges();
+        };
+
+        if (this.xpressEnv) {
+            (<any> window).XPress.registerQXPCallbackHandler(1, 1560, callbackListener);
+        }
     }
 
     tabChangeHandler(currentTab) {
         this.activeTabIndex = currentTab.id;
+    }
+
+    handleTextChange(currentState) {
+        console.log(currentState);
     }
 }
