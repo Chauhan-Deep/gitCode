c381a90fbe7df0ac5b5e95935a8b2ddeb57a2b67
nupur_11 <nbansal@quark.com>
2019-08-23 10:05:59 +0530

    Made use of ng templtae ngIf instead of loading screens via dynamic component loading.

--- xtensions/idml-batch-conversion/src/app/scan-files/scan-files.component.ts
+++ xtensions/idml-batch-conversion/src/app/scan-files/scan-files.component.ts
@@ -8,10 +8,42 @@ import { MatStepper } from '@angular/material/stepper';
 })
 export class ScanFilesComponent implements OnInit {
   @Input() stepper: MatStepper;
+  smartScanWindow: boolean;
+  searchingWindow: boolean;
+  searchResultWindow: boolean;
+  filesListView: boolean;
+  numOfFiles: number;
+  numOfINDDFiles: number;
+  numOfIDMLFiles: number;
 
-  constructor() { }
+  constructor() { 
+    this.smartScanWindow = true;
+    this.searchingWindow = false;
+    this.searchResultWindow = false;
+    this.filesListView = false;
+  }
 
   ngOnInit() {
   }
 
+  showSearchingWindow() {
+    this.smartScanWindow = false;
+    this.searchingWindow = true;
+    this.searchResultWindow = false;
+    this.filesListView = false;
+  }
+
+  performSystemScan() {
+    this.showSearchingWindow();
+    this.numOfFiles = 27;
+    this.numOfINDDFiles = 10;
+    this.numOfIDMLFiles = 17;
+  }
+
+  performCustomScan() {
+    this.showSearchingWindow();
+    this.numOfFiles = 27;
+    this.numOfINDDFiles = 10;
+    this.numOfIDMLFiles = 17;
+  }
 }
