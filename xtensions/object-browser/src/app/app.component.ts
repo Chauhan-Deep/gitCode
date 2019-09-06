c5a6a4504a946dbe622e4b90bc8e9f0fe6a04820
TestWebServer <49711557+TestWebServer@users.noreply.github.com>
2019-09-06 20:07:28 +0530

    Handled docstate change callback to select the corresponding node in the object tree. #92185.

--- xtensions/object-browser/src/app/app.component.ts
+++ xtensions/object-browser/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, NgZone } from '@angular/core';
 import { QxTreeModule } from '@quark/xpressng';
 import { QxTreeComponent, QxTreeNode, QxTreeNodeOptions, QxTreeEmitEvent, QxTreeBeforeDropEvent } from '@quark/xpressng';
 
@@ -10,16 +10,23 @@ import { QxTreeComponent, QxTreeNode, QxTreeNodeOptions, QxTreeEmitEvent, QxTree
 
 
 export class AppComponent {
+  ngZone : NgZone;
+
+  constructor(_ngZone: NgZone) {
+    this.ngZone = _ngZone;
+  }
 
   @ViewChild('qxTreeComponent', { static: false }) qxTreeComponent: QxTreeComponent;
 
   title = 'object-browser';
- 
+  private CHGDOCSTATCallbackHandlerID: number;
+  
   boxNodes = [
     {
       title: 'Root',
       key: '1',
       expanded: true,
+      selected: false,
       children: []
     }
   ];
@@ -49,6 +56,7 @@ export class AppComponent {
     this.boxNodes[0].title = boxName;
     this.boxNodes[0].key = boxID;
     this.AddChidren(this.boxNodes[0].children, boxID);
+    this.docStateChange();
   }
 
   AddChid(nodes, flexboxID)
@@ -61,6 +69,7 @@ export class AppComponent {
         'key': flexboxID,
         'expanded': true,
         "children": [],
+        "selected": false,
         "isLeaf": (childCount == undefined) || childCount == 0
     };
     nodes.push(childdata); 
@@ -86,4 +95,30 @@ export class AppComponent {
     this.boxNodes[0].title = event.node.title;
     (<any>window).app.components.flex.setcurbox(event.node.key);
   }
+
+  docStateChange() { 
+      let _this = this;
+      const callbackListener = (response) => {
+       response = JSON.parse(response);
+        let boxID = (<any>window).app.activeBoxes().boxIDs[0];
+        console.log("boxID="+boxID);
+        this.ngZone.run(() => {  
+        let node = this.qxTreeComponent.getTreeNodeByKey(boxID);
+        if (node) {
+          if (node.isSelectable) {
+            node.isSelected = true; 
+          }
+        }
+      });
+    }
+
+  if ((<any> window).app) {
+      this.CHGDOCSTATCallbackHandlerID = (<any> window).XPress.registerQXPCallbackHandler(0, 668, callbackListener);
+  }
 }
+
+ngOnDestroy() {
+  (<any> window).XPress.deRegisterQXPCallbackHandler(1, 668, this.CHGDOCSTATCallbackHandlerID);
+}
+}
+
