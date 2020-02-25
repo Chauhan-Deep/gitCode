cdc865bca5ff5449bf7513f1e9b37381a8cbd1ab
Vikram Khorana <vkhorana@quark.com>
2020-02-25 14:01:41 +0530

    Display Promo Code.

--- customer-engagements/TrialScreen/src/app/translate/i18n/fr-FR.ts
+++ customer-engagements/TrialScreen/src/app/translate/i18n/fr-FR.ts
@@ -6,9 +6,10 @@ export const LANG_FR_FR_TRANS = {
     'day': '^1 Jour',
     'remainingText': 'restants dans votre essai gratuit.',
     'trialText': 'Votre période d\'essai est sur le point d\'expirer. Visitez notre magasin pour acheter maintenant.',
-    'qxAdvantage': 'Économiser 25% avec QuarkXPress Advantage!',
+    'qxAdvantage': 'Économiser 10% avec QuarkXPress Advantage!',
     'thanksText': 'Merci d\'avoir essayé QuarkXPress.',
     'evalText': 'La période d\'évaluation de cette copie de QuarkXPress a expiré. Visitez notre magasin pour acheter maintenant.',
     'buy': 'Acheter maintenant',
-    'activate': 'Activer la licence'
+    'activate': 'Activer la licence',
+    'coupon': 'Code promo: QXPTRIAL10',
 };
--- customer-engagements/TrialScreen/src/app/translate/i18n/pt-PT.ts
+++ customer-engagements/TrialScreen/src/app/translate/i18n/pt-PT.ts
@@ -6,9 +6,10 @@ export const LANG_PT_PT_TRANS = {
     'day': '^1 Dia',
     'remainingText': 'restantes da sua avaliação gratuita.',
     'trialText': 'A sua avaliação do QuarkXPress está quase a expirar. Visite a nossa loja para comprar agora.',
-    'qxAdvantage': 'Guardar 25% com o QuarkXPress Advantage.',
+    'qxAdvantage': 'Guardar 10% com o QuarkXPress Advantage.',
     'thanksText': 'Obrigado por experimentar o QuarkXPress!',
     'evalText': 'O período de avaliação desta cópia do QuarkXPress terminou. Visite a nossa loja para comprar agora.',
     'buy': 'Comprar agora',
-    'activate': 'Ativar licença'
+    'activate': 'Ativar licença',
+    'coupon': 'Cupão: QXPTRIAL10',
 };
