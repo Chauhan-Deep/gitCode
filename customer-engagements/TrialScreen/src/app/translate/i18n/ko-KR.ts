d09091ab617df944935ab5626c841b88907b250c
Vikram Khorana <vkhorana@quark.com>
2020-05-01 17:27:27 +0530

    Implemented new Trial Screen.

--- customer-engagements/TrialScreen/src/app/translate/i18n/ko-KR.ts
+++ customer-engagements/TrialScreen/src/app/translate/i18n/ko-KR.ts
@@ -9,6 +9,7 @@ export const LANG_KO_KR_TRANS = {
     'qxAdvantage': 'QuarkXPress Advantage로 10%을(를) 저장하십시오.',
     'thanksText': 'QuarkXPress 평가판을 사용해주셔서 감사합니다.',
     'evalText': '본 QuarkXPress의 평가 기간이 만료되었습니다. 지금 구입하려면 스토어에 방문해 주십시오.',
+    'evalTextOnline': '본 QuarkXPress의 평가 기간이 만료되었습니다. 지금 구입하려면 스토어에 방문해 주십시오.',
     'buy': '지금 구입하기',
     'activate': '라이센스 등록 검증',
     'coupon': '쿠폰: QXPTRIAL10',
