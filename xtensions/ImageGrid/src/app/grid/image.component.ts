36812b8b4573a6a3e7a7b67193e55b40ed54b13f
Deepak Kumar Bhartiya <dkbhartiya@quark.com>
2021-09-15 12:19:57 +0000

    Added 4 files to /xtensions/ImageGrid/src/app/grid

---
+++ xtensions/ImageGrid/src/app/grid/image.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, Output,EventEmitter, OnInit  } from '@angular/core';
+
+@Component({
+  selector: 'image-component',
+  templateUrl: './image.component.html',
+  styleUrls: ['./image.component.css']
+})
+
+export class ImageComponent implements OnInit {
+  @Input() imageData: any;
+
+  constructor() 
+  {   
+  }
+
+  ngOnInit(): void {
+    console.log(this.imageData.mUserID);
+  }
+}
