c2a240c610b6e534ff3f2199280e67c89b0eca35
Deepak Kumar Bhartiya <dkbhartiya@quark.com>
2021-09-21 10:50:41 +0000

    Updated image.component.ts

--- xtensions/ImageGrid/src/app/grid/image.component.ts
+++ xtensions/ImageGrid/src/app/grid/image.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output,EventEmitter, OnInit  } from '@angular/core';
+import { Component, Input, Output,EventEmitter, OnInit, HostListener  } from '@angular/core';
 
 @Component({
   selector: 'image-component',
@@ -15,4 +15,16 @@ export class ImageComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  @HostListener("dragstart", ["$event"]) public onDragStart(event:DragEvent) {
+    event.preventDefault();
+    
+    let downloadjson : string = "";
+    
+    downloadjson = "{\"dropImage\":[{\"url\":\"" + this.imageData.mDownloadURL + "\"," + "\"imageprovider\":\"" + this.imageData.mImageProvider + "\"," + "\"imageid\":\"" + this.imageData.mImageID + "\"}]}";
+
+    console.log(JSON.parse(downloadjson));
+
+    window?.chrome?.webview?.postMessage(downloadjson);
+   }
 }
