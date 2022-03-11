bb688f8ce39781924fe679a594bb9578d3ae3649
Deepak Kumar Bhartiya <dkbhartiya@quark.com>
2022-03-11 20:48:24 +0530

    Added Ok and Cancel button.

--- xtensions/StockImageGrid/src/app/collection-list/collection-list.component.ts
+++ xtensions/StockImageGrid/src/app/collection-list/collection-list.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./collection-list.component.css']
 })
 export class CollectionsComponent implements OnInit {
-  collections: string[] = ['Flowers', 'Tree', 'Water-Fall'];
+  collections: string[] = ['Flowers', 'Tree', 'Water-Fall', 'Flowers1', 'Tree1', 'Water-Fall1', 'Flower2s', 'Tree2', 'Water-Fall2'];
 
   constructor() { }
 
